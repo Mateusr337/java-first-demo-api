dc4095b33c9093d5794bc2e87930ae50501e8676
Rossetto <mateus.rosseto@capco.com>
2022-09-06 15:58:27 -0300

    Initial commit

---
+++ src/main/java/com/example/demo/model/User.java
@@ -0,0 +1,25 @@
+package com.example.demo.model;
+
+import javax.persistence.*;
+
+@Entity (name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue (strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column (nullable = false)
+    private String name;
+
+    public Long getId () {
+
+        return id;
+    }
+
+    public String getName () {
+
+        return name;
+    }
+
+}
